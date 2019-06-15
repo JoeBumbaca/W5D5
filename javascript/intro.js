d623b357876f61697e05e3fa5a67826e1ac35cbe
JoeBumbaca <joebumbaca@gmail.com>
2019-06-14 17:05:42 -0700

    Complete addNumber and paritally finished bubble sort

--- javascript/intro.js
+++ javascript/intro.js
@@ -56,4 +56,51 @@ function addNumbers(sum, numsLeft, completionCallback) {
   }
 }
 
-addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+  function outerBubbleSortLoop(madeAnySwaps = true) {
+    if (madeAnySwaps === true) {
+      innerBubbleSortLoop(arr, i = 0, madeAnySwaps, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback();
+    }
+  }
+}
+
+function askIfGreaterThan(el1, el2, callback) {
+  reader.question(`Is ${el1} greater than ${el2}?`, (res) => {
+    if (res === 'yes') {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+}
+
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  madeAnySwaps = false;
+  if (i == arr.length - 1) {
+    outerBubbleSortLoop(madeAnySwaps);
+  } else {
+    askIfGreaterThan(arr[i], arr[i + 1], isGreaterThan => {
+      if (isGreaterThan === true) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        madeAnySwaps = true;
+        i++;
+        innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop);
+      } else {
+        i++;
+        innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop);
+      }
+    });
+  }
+}
+
+absurdBubbleSort([3, 2, 1], function (arr) {
+  console.log("Sorted array: " + JSON.stringify(arr));
+  reader.close();
+});
+
